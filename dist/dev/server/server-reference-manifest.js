387ce4e0d88fef5c3fa381622a75c23b71a67614
Pigman <pigman@dev.local>
2026-04-27 14:26:22 +0800

    chore: add images.unoptimized for static export (Cloudflare Pages compat)

--- dist/dev/server/server-reference-manifest.js
+++ dist/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZkrdWlnhg5o1gb549RvWB7RJJ0rV3u+oQBPhWbnrKR8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"G1+ih0KvudSEZOqzzcllpCklrSNnfO8Q72702/t2YAs=\"\n}"
